bac0e49be42ac48a7c25e3fb0c3ef864fa292fea
yun-sangho <contact@yunsangho.com>
2019-02-19 20:36:34 +0900

    refactor circleci

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "extends": "standard",
-    "rules": {
-        "no-undef-init": 0,
-        "semi": [2, 'always']
-    }
-};
--- spec/BoardViewSpec.js
+++ spec/BoardViewSpec.js
@@ -1,11 +1,11 @@
-describe('ChessboardView', function() {
+describe("ChessboardView", function() {
   var view;
 
   beforeEach(function() {
-    view = new BoardView({model: new Board({n: 4})});
+    view = new BoardView({ model: new Board({ n: 4 }) });
   });
 
-  it('should exist', function() {
+  it("should exist", function() {
     expect(view).to.be.ok;
   });
 });
